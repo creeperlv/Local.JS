7bbf98816023f6c3d8e05aeed9644480280c1d07
Creeper Lv <creeperlv38@outlook.com>
2021-05-28 11:13:58 +0800

    Working on integrating Terminal.Gui (gui.cs) into an extension of Loacl.JS.

---
+++ Local.JS.Extension.TerminalGUI/GUICS.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Terminal.Gui;
+
+namespace Local.JS.Extension.TerminalGUI
+{
+    public static class GUICS
+    {
+        static GUICS()
+        {
+            var asm=AppDomain.CurrentDomain.GetAssemblies();
+            foreach (var item in asm)
+            {
+                try
+                {
+                    if (item.GetType("Terminal.Gui.Application") is not null)
+                    {
+                        return;
+                    }
+                }
+                catch (Exception)
+                {
+
+                }
+            }
+            var f = new FileInfo(typeof(GUICS).Assembly.Location);
+            Assembly.LoadFrom(Path.Combine(f.Directory.FullName, "Terminal.Gui.dll"));
+        }
+        static Toplevel CurrentTopLevel;
+        public static void Init()
+        {
+            Application.Init();
+            CurrentTopLevel = Application.Top;
+        }
+        public static Toplevel GetTop() => CurrentTopLevel;
+        public static Window CreateNewWindow(string Title,int X,int Y,Dim Width,Dim Height)
+        {
+            Window window = new Window();
+            window.Title = Title;
+            window.X = X;
+            window.Y = Y;
+            window.Width = Width;
+            window.Height=Height;
+            return window;
+        }
+
+    }
+}
