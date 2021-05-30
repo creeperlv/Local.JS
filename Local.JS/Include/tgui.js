45450f1c43de3d574457bdef805375bd918fd749
Creeper Lv <creeperlv38@outlook.com>
2021-05-30 16:35:58 +0800

    Added check if TerminalGUI is enabled to `window.js`. Working on `tgui.js`

---
+++ Local.JS/Include/tgui.js
@@ -0,0 +1,10 @@
+﻿/// DEFINE AUTHOR "Creeper Lv"
+/// DEFINE NAME "TerminalGUI"
+/// DEFINE VERSION 0.1.0.0
+/// USING DLL Local.JS.Extension.TerminalGUI.dll
+/// EXPOSETYPE Index Local.JS.Extension.TerminalGUI.GUICS
+var TerminalGUIInited = false;
+function InitGUI() {
+    GUICS.Init();
+    TerminalGUIInited = true;
+}
--- Local.JS/Include/window.js
+++ Local.JS/Include/window.js
@@ -6,14 +6,45 @@
 /// DEFINE WINDOWOBJ
 var window = new Object();
 /// ENDIF
+alert = function __alert(text) {
+    var b0 = typeof (TerminalGUIInited) == "undefined";
+    if (b0 == true) {
+        Console.WriteLine(text);
+    } else {
+        if (TerminalGUIInited == false) {
+            Console.WriteLine(text);
+        } else {
+            //Will work...
+
+        }
+    }
+
+}
 window.alert = alert;
 function prompt(text, value) {
-    alert("=INPUT====");
-    alert("=" + text);
-    alert("=Default Value:" + value);
-    var a = Console.ReadLine();
-    if (a == "") return value;
-    else return a;
+    var b0 = typeof (TerminalGUIInited) == "undefined";
+    if (b0 == true) {
+
+        alert("=INPUT====");
+        alert("=" + text);
+        alert("=Default Value:" + value);
+        var a = Console.ReadLine();
+        if (a == "") return value;
+        else return a;
+    } else {
+        if (TerminalGUIInited == false) {
+
+            alert("=INPUT====");
+            alert("=" + text);
+            alert("=Default Value:" + value);
+            var a = Console.ReadLine();
+            if (a == "") return value;
+            else return a;
+        } else {
+            //Will work...
+            return "";
+        }
+    }
 };
 
 window.prompt = prompt;
